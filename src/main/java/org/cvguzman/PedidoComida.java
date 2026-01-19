d3228e30ceb1e46e1abdd232a199e375f779544e
cvguzman <157186909+cvguzman@users.noreply.github.com>
2026-01-19 18:21:50 -0300

    se agrega método mostrarResumen() el cual muestra info lógica del pedido "Comida" se implementa método abstracto en protected calcularTiempoEntrega()

--- src/main/java/org/cvguzman/PedidoComida.java
+++ src/main/java/org/cvguzman/PedidoComida.java
@@ -2,8 +2,17 @@ package org.cvguzman;
 
 public class PedidoComida extends Pedido {
 
+    public PedidoComida(int distanciaKm){
+        super(distanciaKm);
+    }
+
     @Override
-    public void asignarRepartidor(String nombreRepartidor) {
-        System.out.println("Repartidor asignado para comida con mochila térmica " + nombreRepartidor);
+    public void mostrarResumen(String nombreRepartidor) {
+        System.out.println("Pedido número 001 tipo comida con mochila térmica  ha siido asignado al repartidor: " + nombreRepartidor);
+    }
+
+    @Override
+    protected int calcularTiempoEntrega(){
+        return (int) (15 + 2 * getDistanciaKm());
     }
 }
