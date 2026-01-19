0bfbdd9ff176ecac592c2e58a533185f12b9f0f7
cvguzman <157186909+cvguzman@users.noreply.github.com>
2026-01-19 18:20:57 -0300

    Clase base Pedido pasa a ser abstracta se crea método mostrarResumen() el cual muestra info básica del pedido se implementa método abstracto en protected calcularTiempoEntrega() para ser incorporado en las clases hijas

--- src/main/java/org/cvguzman/Pedido.java
+++ src/main/java/org/cvguzman/Pedido.java
@@ -1,12 +1,31 @@
 package org.cvguzman;
 
-public class Pedido {
-    public void asignarRepartidor() {
+public abstract class Pedido {
+    private int idPedido;
+    private String direccionEntrega;
+    private int distanciaKm;
+
+    public Pedido(int idPedido) {
+        this.idPedido = idPedido;
+        this.direccionEntrega = direccionEntrega;
+        this.distanciaKm = distanciaKm;
+    }
+
+    public int getDistanciaKm(){
+        return distanciaKm;
+    }
+
+    public void mostrarResumen() {
         System.out.println("Un repartidor ha sido asignado");
     }
 
-    public void asignarRepartidor(String nombreRepartidor) {
+    public void mostrarResumen(String nombreRepartidor) {
         System.out.println("Asignación para " + nombreRepartidor);
+    }
+
+    protected int calcularTiempoEntrega(){
+        System.out.println("Tiempo estimado de entrega");
+        return 0;
     }
 }
 
