4abfa1098a9e3c0ad37288e299c8dc64c56cf6e1
cvguzman <157186909+cvguzman@users.noreply.github.com>
2026-01-19 18:22:08 -0300

    se agrega método mostrarResumen() el cual muestra info lógica del pedido "Encomienda" se implementa método abstracto en protected calcularTiempoEntrega()

--- src/main/java/org/cvguzman/PedidoEncomienda.java
+++ src/main/java/org/cvguzman/PedidoEncomienda.java
@@ -1,8 +1,18 @@
 package org.cvguzman;
 
 public class PedidoEncomienda extends Pedido {
+
+    public PedidoEncomienda(int distanciaKm){
+        super(distanciaKm);
+    }
+
     @Override
-    public void asignarRepartidor(String nombreRepartidor) {
-        System.out.println("Repartidor asignado para encomienda validando peso y embalaje " + nombreRepartidor);
+    public void mostrarResumen(String nombreRepartidor) {
+        System.out.println("Pedido número 002 tipo encomienda validando peso y embalaje ha sido asignado a repartidor: " + nombreRepartidor);
+    }
+
+    @Override
+    protected int calcularTiempoEntrega(){
+        return (int) Math.round(20 + 1.5 * getDistanciaKm());
     }
 }
