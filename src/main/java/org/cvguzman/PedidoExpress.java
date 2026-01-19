c48eb071af5d61836a8795194f172bfbe6ace5e0
cvguzman <157186909+cvguzman@users.noreply.github.com>
2026-01-19 18:22:21 -0300

    se agrega método mostrarResumen() el cual muestra info lógica del pedido "Express" se implementa método abstracto en protected calcularTiempoEntrega()

--- src/main/java/org/cvguzman/PedidoExpress.java
+++ src/main/java/org/cvguzman/PedidoExpress.java
@@ -1,8 +1,24 @@
 package org.cvguzman;
 
 public class PedidoExpress extends Pedido {
+
+    public PedidoExpress(int distanciaKm) {
+        super(distanciaKm);
+    }
+
     @Override
-    public void asignarRepartidor(String nombreRepartidor){
-        System.out.println("Repartidor ha sido asignado para pedido express con disponibilidad inmediata y mas cercano " + nombreRepartidor);
+    public void mostrarResumen(String nombreRepartidor) {
+        System.out.println("Pedido número 003 tipo express con disponibilidad inmediata y más cercano ha sido asignado a repartidor: " + nombreRepartidor);
+    }
+
+    @Override
+    protected int calcularTiempoEntrega() {
+        int tiempo = 10;
+        if (getDistanciaKm() > 5) {
+            tiempo += 5;
+        }
+        return tiempo;
     }
 }
+
+
