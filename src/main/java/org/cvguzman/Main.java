715d5362325ac297e0a0d3809bfaaa282eb44271
cvguzman <157186909+cvguzman@users.noreply.github.com>
2026-01-25 19:52:55 -0300

    Se modifican objetos instanciados con constructor vacio  y evitar exceso de código

--- src/main/java/org/cvguzman/Main.java
+++ src/main/java/org/cvguzman/Main.java
@@ -8,14 +8,10 @@ public class Main {
         System.out.println(":::::::PRUEBA POLIMORFISMO ---> SOBRESCRITURA::::::");
         System.out.println(" ");
 
-        Pedido[] pedidos = {
-                new PedidoComida(001,"Avenida Suecia 15, Providencia", 15),
-                new PedidoEncomienda(002,"Tajamar 75, Providencia",6),
-                new PedidoExpress(003,"Av Tobalaba 4556, Las Condes",17),
-        };
+        Pedido[] pedidos = { new PedidoComida(), new PedidoEncomienda(), new PedidoExpress()};
 
         for (Pedido p : pedidos) {
-            p.mostrarResumen();
+            p.asignarRepartidor();
         }
 
         System.out.println(" ");
@@ -23,23 +19,22 @@ public class Main {
         System.out.println("::::::PRUEBA SOBRECARGA  ---->  TEMPLATE METHOD::::::");
         System.out.println(" ");
 
-        PedidoComida repartidor = new PedidoComida(001,"Avenida Suecia 15, Providencia",15);
+        PedidoComida repartidor = new PedidoComida(1,"Avenida Suecia 15, Providencia","Comida",15);
         repartidor.mostrarResumen("Mauricio Pérez");
-        System.out.println("Distancia: " + repartidor.getDistanciaKm());
+        System.out.println("Distancia en km: " + repartidor.getDistanciaKm());
         System.out.println("Tiempo de entrega estimado en minutos: "  + repartidor.calcularTiempoEntrega());
 
         System.out.println(" ");
-        PedidoEncomienda repartidor2 = new PedidoEncomienda(002,"Tajamar 75, Providencia",6);
+        PedidoEncomienda repartidor2 = new PedidoEncomienda(2,"Tajamar 75, Providencia","Encomienda", 6);
         repartidor2.mostrarResumen("Soraya Marín");
-        System.out.println("Distancia: " + repartidor2.getDistanciaKm());
+        System.out.println("Distancia en km: " + repartidor2.getDistanciaKm());
         System.out.println("Tiempo de entrega estimado en minutos: "  + repartidor2.calcularTiempoEntrega());
 
 
         System.out.println(" ");
-        PedidoExpress repartidor3 = new PedidoExpress(003,"Av Tobalaba 4556, Las Condes",17);
+        PedidoExpress repartidor3 = new PedidoExpress(3,"Av Tobalaba 4556, Las Condes","Express", 17);
         repartidor3.mostrarResumen("Patricio Solís");
-        System.out.println("Distancia: " + repartidor3.getDistanciaKm());
+        System.out.println("Distancia en km: " + repartidor3.getDistanciaKm());
         System.out.println("Tiempo de entrega estimado en minutos: " + repartidor3.calcularTiempoEntrega());
-
     }
 }
