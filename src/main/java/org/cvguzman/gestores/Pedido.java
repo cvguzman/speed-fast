7e1e4207d7357a8cf061b3c571c76d74a39e7de8
cvguzman <157186909+cvguzman@users.noreply.github.com>
2026-02-07 23:44:03 -0300

    Se modifica Pedido para obtener un toString y metodos especificos

--- src/main/java/org/cvguzman/gestores/Pedido.java
+++ src/main/java/org/cvguzman/gestores/Pedido.java
@@ -1,104 +1,54 @@
 package org.cvguzman.gestores;
 
-import org.cvguzman.interfaces.Cancelable;
-import org.cvguzman.interfaces.Despachable;
-import org.cvguzman.interfaces.Rastreable;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Pedido implements Cancelable, Despachable, Rastreable {
-    private int idPedido;
+public class Pedido {
+    private int id;
     private String direccionEntrega;
-    private String tipoPedido;
-    protected int distanciaKm;
+    private EstadoPedido estado;
 
-    private List<String> historial = new ArrayList<>();
 
-    @Override
-    public String cancelar(String motivoCancelacion) {
-        String evento = "Pedido cancelado: " + motivoCancelacion;
-        historial.add(evento);
-        return evento;
-    }
+    public Pedido(int id, String direccionEntrega, int i) {};
 
-   @Override
-   public String despachar(String modoDespacho) {
-        String evento = "Pedido despachado: " + modoDespacho;
-        historial.add(evento);
-        return evento;
-   }
-
-   @Override
-    public List<String> verHistorial() {
-        return historial;
-    }
-
-    public Pedido(){}
-
-    public Pedido(int idPedido, String direccionEntrega, String tipoPedido, int distanciaKm) {
-        this.idPedido = idPedido;
+    public Pedido(int id, String direccionEntrega, EstadoPedido estadoPedido) {
+        this.id = id;
         this.direccionEntrega = direccionEntrega;
-        this.tipoPedido = tipoPedido;
-        this.distanciaKm = distanciaKm;
+        this.estado = EstadoPedido.PENDIENTE;
     }
 
-    public int getIdPedido(){
-        return idPedido;
+    public int getId() {
+        return id;
     }
 
-    public String getDireccionEntrega(){
+    public String getDireccionEntrega() {
         return direccionEntrega;
     }
 
-    public String getTipoPedido(){
-        return tipoPedido;
+    public EstadoPedido getEstadoPedido() {
+        return estado;
     }
 
-    public int getDistanciaKm(){
-        return distanciaKm;
+    public void setId() {
+        this.id = id;
     }
 
-    public void asignarRepartidor(){
-        System.out.println("Un repartidor ha sido asigmado");
+    public void setDireccionEntrega() {
+        this.direccionEntrega = direccionEntrega;
     }
 
-    public void asignarRepartidor(String nombreRepartidor){
-        System.out.println(
-                                  "El número pedido "
-                                  + idPedido
-                                  + " con dirección de entrega: "
-                                  + direccionEntrega
-                                  + " clasificado como tipo pedido "
-                                  + tipoPedido
-                                  + " es asignador al repartidor: "
-                                  + nombreRepartidor
-                                   );
+    public void setEstadoPedido() {
+        this.estado = estado;
     }
 
-    public String mostrarResumen() {
-        System.out.println("El pedido debe contener su número, dirección, tipo de pedido y nombre repartidor");
-        return null;
+    @Override
+    public String toString(){
+        return  "Pedido{" +
+                        "id=" + id +
+                        ", direccionEntrega' " + direccionEntrega + " \'" +
+                        ", estadoPedido=" + estado +
+                        '}';
     }
 
-    public void mostrarResumen(String nombreRepartidor){
-        System.out.println(
-                "El número pedido "
-                        + getIdPedido()
-                        + " con dirección de entrega: "
-                        + getDireccionEntrega()
-                        + " ha clasificado como tipo pedido: "
-                        + getTipoPedido() + " asignado al repartidor: "
-                        + nombreRepartidor);
-    }
-
-
-    public int calcularTiempoEntrega() {
-        return 0;
-    };
-
-    public void despachar() {
-        return;
+    public void setEstado(EstadoPedido nuevoEstado) {
+        this.estado = estado;
     }
 }
 
