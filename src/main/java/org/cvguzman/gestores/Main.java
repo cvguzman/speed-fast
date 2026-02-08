f95edabd7093f500f210ef1e13d8845dd8858c3e
cvguzman <157186909+cvguzman@users.noreply.github.com>
2026-02-07 23:43:25 -0300

    Se modifica main para ejecutar hilos

--- src/main/java/org/cvguzman/gestores/Main.java
+++ src/main/java/org/cvguzman/gestores/Main.java
@@ -1,89 +1,34 @@
 package org.cvguzman.gestores;
 
-import org.cvguzman.subclases.PedidoComida;
-import org.cvguzman.subclases.PedidoEncomienda;
-import org.cvguzman.subclases.PedidoExpress;
-
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(" ");
+        ZonaDeCarga zonaDeCarga = new ZonaDeCarga();
 
-        System.out.println(":::::::PRUEBA POLIMORFISMO ---> SOBRESCRITURA::::::");
-        System.out.println(" ");
-
-        Pedido[] pedidos = {new PedidoComida(), new PedidoEncomienda(), new PedidoExpress() {
-            @Override
-            public void despachar() {
-
-            }
-        }};
-
-        for (Pedido p : pedidos) {
-            p.asignarRepartidor();
+        for (int i = 1; i <= 5; i++) {
+            zonaDeCarga.agregarPedido(
+                    new Pedido(i, "Las Torres 15, Peñalolen", 1)
+            );
         }
 
-        System.out.println(" ");
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-        System.out.println("::::::PRUEBA INTERFACES::::::");
-        System.out.println(" ");
+        executorService.execute(new Repartidor("Repartidor Marcelo", zonaDeCarga));
+        executorService.execute(new Repartidor("Repartidor Luis", zonaDeCarga));
+        executorService.execute(new Repartidor("Repartidor Gerardo", zonaDeCarga));
 
-        PedidoComida repartidor = new PedidoComida(
-                1,
-                "Avenida Suecia 15, Providencia \n",
-                "Comida \n",
-                15);
-        repartidor.mostrarResumen("Mauricio Pérez");
-        System.out.println(repartidor.cancelar("Se ha cancelado Pedido Comida por no tener menú disponible"));
-        repartidor.despachar("Se ha despachado Pedido de Comida correctamente a destino");
+        executorService.shutdown();
 
-
-        System.out.println(" ");
-        PedidoEncomienda repartidor2 = new PedidoEncomienda(
-                2,
-                "Tajamar 75, Providencia \n",
-                "Encomienda \n",
-                6);
-        repartidor2.mostrarResumen("Soraya Marín");
-        System.out.println(repartidor2.despachar("Se ha despachado Pedido Encomienda correctamente"));
-        System.out.println("Distancia en km: " + repartidor2.getDistanciaKm());
-        System.out.println("Tiempo de entrega estimado en minutos: " + repartidor2.calcularTiempoEntrega());
-
-
-        System.out.println(" ");
-        PedidoExpress repartidor3 = new PedidoExpress(
-                3,
-                "Av Tobalaba 4556, Las Condes \n",
-                "Express \n",
-                17);
-
-        System.out.println("El pedido número " + repartidor3.getIdPedido());
-        System.out.println(repartidor3.cancelar("Se ha cancelado Pedido Express por no haber repartidor cerca y con disponibilidad inmediata"));
-
-
-        System.out.println(" ");
-        System.out.println("::::::HILOS::::::");
-
-        Pedido p1 = new PedidoComida(1, "Av Central 3", "Comida", 6);
-        Pedido p2 = new PedidoExpress(2, "Av Suecia 23", "Express", 4);
-
-        Pedido p3 = new PedidoEncomienda(5, "Torre 33", " Encomienda", 6);
-        Pedido p4 = new PedidoComida(8, "Las Tacas 22", "Comida", 5);
-
-        Pedido p5 = new PedidoExpress(10, "Los nidos 43", "Express", 3);
-        Pedido p6 = new PedidoExpress(12, "Los cilos 3", "Express", 3);
-
-
-        Repartidor r1 = new Repartidor("Carlos", List.of (p1,p2) );
-        Repartidor r2 = new Repartidor( "Luis", List.of(p3, p4));
-        Repartidor r3 = new Repartidor( "Mauricio", List.of(p5, p6));
-
-        r1.start();
-        r2.start();
-        r3.start();
-
+        try {
+            executorService.awaitTermination(1, TimeUnit.MINUTES);
+        }catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
+        System.out.println("Los pedidos han sido repartidos correctamente");
     }
+}
 
